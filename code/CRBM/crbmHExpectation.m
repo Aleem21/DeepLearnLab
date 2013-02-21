5333f4b752f668676702fe2525071ab0c0186cb6
Dan Howarth <d.c.howarth@gmail.com>
2013-02-21 16:07:12 -0500

    fixed bias vars

--- code/CRBM/crbmHExpectation.m
+++ code/CRBM/crbmHExpectation.m
@@ -8,14 +8,16 @@ h_expected_k = cell(1, crbm.n_maps);
 W = crbm.W;
 hbias = crbm.h_b;
 
-parfor k=1:crbm.n_maps
+for k=1:crbm.n_maps
+%parfor k=1:crbm.n_maps
     h_expected_k{k} = convcut(visible, kthMap(W,k)') + hbias(1,k);
 end
 
 s = size(h_expected_k{1});
 h_expected = zeros([s(1:end-1) crbm.n_maps s(end)]); 
 
-parfor k=1:crbm.n_maps
+for k=1:crbm.n_maps
+%parfor k=1:crbm.n_maps
     h_expected(:,:,k,:) = h_expected_k{k};
 end
 
