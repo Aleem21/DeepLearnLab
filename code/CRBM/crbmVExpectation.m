d55997a88af479071231a953320b144812e9845e
Dan Howarth <d.c.howarth@gmail.com>
2013-02-21 16:05:00 -0500

    Change convcut to convn

--- code/CRBM/crbmVExpectation.m
+++ code/CRBM/crbmVExpectation.m
@@ -4,7 +4,7 @@ function [ v_expected ] = crbmVExpectation( crbm, hidden )
 % MATLAB suggested this to reduce overhead in parfor.
 W = crbm.W;
 
-v_expected = convcut(hidden, W);
+v_expected = convn(hidden, W, 'full');
 
 k_dim = length(size(W)) -1;
 v_expected = sigmoid(sum(v_expected, k_dim)+crbm.v_b);
