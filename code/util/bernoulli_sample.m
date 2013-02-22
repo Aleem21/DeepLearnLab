473e8b6a8e788dee7c0c740f0714e40bb1801bc9
Dan Howarth <d.c.howarth@gmail.com>
2013-02-22 11:12:16 -0500

    bernoulli_sample now a bajillion times faster.

--- code/util/bernoulli_sample.m
+++ code/util/bernoulli_sample.m
@@ -1,14 +1,4 @@
 function [ sample ] = bernoulli_sample( p )
-
-    function s = bs(p_)
-        if rand(1,1) < p_
-            s = 1;
-        else
-            s = 0;
-        end
-    end
-
-    sample = arrayfun(@(x) bs(x),p);
-
+    sample = rand(size(p)) < p;
 end
 
