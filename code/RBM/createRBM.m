81ae89ce3242ce01fdc84c203d6033dd345ee5b9
Dan Howarth <d.c.howarth@gmail.com>
2013-04-27 19:52:28 -0400

    Code Refactored.  Added documentation to DBN portion of code.

--- code/RBM/createRBM.m
+++ code/RBM/createRBM.m
@@ -1,7 +1,22 @@
 function [ rbm ] = createRBM( n_v, n_h, t_v, scale, sparsity, sparsity_decay)
-%CREATE_RBM Summary of this function goes here
-%   Detailed explanation goes here
+% createRBM( n_v, n_h, t_v, scale, sparsity, sparsity_decay) creates a 
+% definition of an RBM model based on the given parameters
+% 
+% INPUTS:
+%   n_v.............: number of vissible units
+%   n_h.............: number of hidden units
+%   t_v.............: type of visible units
+%                       possible types:
+%                           'binary'
+%                           'gauss'
+%   scale...........: scale of initial weights
+%   sparsity........: desired sparsity level of hidden units
+%   sparsity_decay..: sparsity decay value, in range [0, 1]
+%
+% OUTPUTS:
+%   rbm.....: an instatiation of a rbm model given the inputs    
 
+% Set the model's variables
 rbm.n_v = n_v;
 rbm.n_h = n_h;
 rbm.t_v = t_v;
@@ -9,6 +24,7 @@ rbm.scale = scale;
 rbm.sparsity = sparsity;
 rbm.sparsity_decay = sparsity_decay;
 
+% Randomly generate biases and weights
 rbm.v_b = scale * rand(1,n_v);
 rbm.h_b = 2 * sparsity * rand(1,n_h);
 rbm.W = scale * rand(n_h, n_v);
